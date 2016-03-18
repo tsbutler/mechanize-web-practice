2e575ecebec2db1cb6171f735296c7db303a8f27
Thorin Butler <thorin.butler@gmail.com>
2016-03-18 09:06:21 -0500

    added some commentary on a few of the methods

--- mechanize_practice.rb
+++ mechanize_practice.rb
@@ -6,10 +6,21 @@
 
 # puts page.title
 
+# --------------------------
+
 require 'mechanize'
 
 mechanize = Mechanize.new
 
-page = mechanize.get('http://google.com')
+page = mechanize.get('http://bbc.co.uk/news')
 
-puts page.title
+# puts page.title
+
+puts page.at('.buzzard h3').text.strip
+
+# The #at method takes a CSS selector string and returns the first matching 
+# node, in this case an h2 element. The #text method returns the textual 
+# content inside the element, in this case the title of the top story. Try 
+# changing the selector to see what other elements you can pick out of the 
+# page. (The preceding was lifted from the website I found this tutorial 
+# http://readysteadycode.com/howto-scrape-websites-with-ruby-and-mechanize as # is most of this.)
