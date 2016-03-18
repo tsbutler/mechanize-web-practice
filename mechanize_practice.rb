4f80dfcce6e9bbef2887ebe49d74eff883373172
Thorin Butler <thorin.butler@gmail.com>
2016-03-18 09:17:20 -0500

    did as advertised, followed a link

--- mechanize_practice.rb
+++ mechanize_practice.rb
@@ -12,11 +12,11 @@ require 'mechanize'
 
 mechanize = Mechanize.new
 
-page = mechanize.get('http://bbc.co.uk/news')
+# page = mechanize.get('http://bbc.co.uk/news')
 
 # puts page.title
 
-puts page.at('.buzzard h3').text.strip
+# puts page.at('.buzzard h3').text.strip
 
 # The #at method takes a CSS selector string and returns the first matching 
 # node, in this case an h2 element. The #text method returns the textual 
@@ -24,3 +24,16 @@ puts page.at('.buzzard h3').text.strip
 # changing the selector to see what other elements you can pick out of the 
 # page. (The preceding was lifted from the website I found this tutorial 
 # http://readysteadycode.com/howto-scrape-websites-with-ruby-and-mechanize as # is most of this.)
+
+page = mechanize.get('http://en.wikipedia.org/wiki/Main_Page')
+
+link = page.link_with(text: 'Random article')
+
+page = link.click
+
+puts page.uri
+
+# The #link_with method is provided by mechanize, and makes it easy to pull out
+# the random article link. The #click method instructs mechanize to follow the # link, and the #uri method returns the address of the page. Notice that 
+# mechanize follows redirects automatically, so this example makes three HTTP # requests in total.
+
